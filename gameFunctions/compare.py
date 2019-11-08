09f9913bedc63e431e87ea6c6c092281bc92bd5b
Hodges <k_hodges103098@ats-hr420-dm06.fc.ca>
2019-11-08 14:50:08 -0500

    compare

---
+++ gameFunctions/compare.py
@@ -0,0 +1,37 @@
+from gameFunctions import gameVars
+
+# need a function to run to compare the choices
+# # figure out wat to pass into the function - hint => what are you comparing?
+def compare(player):
+	
+	if player == gameVars.computer:
+		#we have a tie, no point going any further
+		print("tie, no one wins! try again")
+
+	elif player =="quit":
+		print("you chose to quit, quitter.")
+		exit()
+
+	elif player == "rock": 
+		if gameVars.computer == "paper":
+			print("You lose!", gameVars.computer, "covers", player, "\n")
+			gameVars.player_lives = gameVars.player_lives -1
+		else:
+			print("You won!", player, "smashes", gameVars.computer, "\n")
+			gameVars.computer_lives = gameVars.computer_lives -1
+
+	elif player == "paper": 
+		if gameVars.computer == "scissors":
+			print("You lose!", gameVars.computer, "cuts", player, "\n")
+			gameVars.player_lives = gameVars.player_lives -1
+		else:
+			print("You won!", player, "covers", gameVars.computer, "\n")
+			gameVars.computer_lives = gameVars.computer_lives -1
+
+	elif player == "scissors": 
+		if gameVars.computer == "rock":
+			print("You lose!", gameVars.computer, "smashes", player, "\n")
+			gameVars.player_lives = gameVars.player_lives -1
+		else:
+			print("You won!", player, "cuts", gameVars.computer, "\n")
+			gameVars.computer_lives = gameVars.computer_lives -1
